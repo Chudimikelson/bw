0706af373d281e63b44d8c982cdbc3203ed4292b
mike oselle <osellezino@outlook.com>
2020-01-22 10:29:49 +0100

    added bootstrap and reactstrap

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <h3>this is the Header</h3>
+      <div>
+        <h3>this is the menu</h3>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/Standings.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Standings() {
+  return (
+    <div>
+      <h3>this is where to put the table/standings</h3>
+    </div>
+  )
+}
